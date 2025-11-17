9316e24f854802fddddd4d6fa14b5950c647811f
DiegoBuencuero <diegobuencuero4095@gmail.com>
2025-11-17 13:53:15 -0300

    base

--- projeto/settings.py
+++ projeto/settings.py
@@ -104,12 +104,23 @@ WSGI_APPLICATION = 'projeto.wsgi.application'
 #     }
 
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3"
+#     }
+# }
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3"
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'IsraelAdolfo$default',
+        'USER': 'IsraelAdolfo',
+        'PASSWORD': '$Emagrecimiento$2025',
+        'HOST': 'IsraelAdolfo.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
